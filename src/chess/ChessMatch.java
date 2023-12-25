0b556f08e6b2c36d0fb8128bc576dad0879f3f1b
Gabriel Laet <51339430+Gabriel-Laet-DEV@users.noreply.github.com>
2023-12-25 19:59:28 -0300

    Better printing board

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -26,13 +26,24 @@ public class ChessMatch {
         return mat;
     }
 
-    private void placeNewPeace(char column, int row, ChessPiece piece) throws ChessException, PositionNotFoundException, BoardException {
+    private void placeNewPiece(char column, int row, ChessPiece piece) throws ChessException, PositionNotFoundException, BoardException {
         board.placePiece(piece, new ChessPosition(column, row).toPosition());
     }
 
     private void inicialSetup() throws BoardException, PositionNotFoundException, ChessException {
-        placeNewPeace('b', 6, new Rook(board, Color.WHITE));
-        placeNewPeace('e', 8, new King(board, Color.BLACK));
-        placeNewPeace('e', 1, new King(board, Color.WHITE));
+        placeNewPiece('c', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('c', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 1, new King(board, Color.WHITE));
+
+        placeNewPiece('c', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('c', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 8, new King(board, Color.BLACK));
+
     }
 }
