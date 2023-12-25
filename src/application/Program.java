d59c26e5be4d31b6ff2fdb6a7e0daf0874c78c2b
Gabriel Laet <51339430+Gabriel-Laet-DEV@users.noreply.github.com>
2023-12-25 17:52:19 -0300

    Placing pieces on the board

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,7 +3,7 @@ package application;
 import boardgame.Position;
 import chess.ChessMatch;
 
-public class Program {
+public class    Program {
     public static void main(String[] args) {
 
         ChessMatch chessMatch = new ChessMatch();
--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -16,17 +16,11 @@ public class Board {
         return rows;
     }
 
-    public void setRows(int rows) {
-        this.rows = rows;
-    }
 
     public int getColumns() {
         return columns;
     }
 
-    public void setColumns(int columns) {
-        this.columns = columns;
-    }
 
     public Piece piece(int row, int column){
         return pieces[row][column];
@@ -35,4 +29,10 @@ public class Board {
     public Piece piece(Position position){
         return pieces[position.getRow()][position.getColumn()];
     }
+
+    public void placePiece(Piece piece, Position position){
+        pieces[position.getRow()][position.getColumn()] = piece;
+        piece.position = position;
+    }
+
 }
